f24ca514d66763e01dd368f7c87f20a505179d52
Tetyana Shvets <tshvets@hotmail.com>
2019-05-17 12:53:30 -0700

    Pagination

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -17,7 +17,8 @@ function LinkItem({ link, index, showCount, history }) {
           const previousVotes = doc.data().votes;
           const vote = { votedBy: { id: user.uid, name: user.displayName } };
           const updatedVotes = [...previousVotes, vote];
-          voteRef.update({ votes: updatedVotes });
+          const voteCount = updatedVotes.length;
+          voteRef.update({ votes: updatedVotes, voteCount });
         }
       })
     }
@@ -37,7 +38,7 @@ function LinkItem({ link, index, showCount, history }) {
   return (
     <div className="flex items-start mt2">
       <div className="flex items-center">
-        {showCount && <span className="gray">{index}</span>}
+        {showCount && <span className="gray">{index}.</span>}
         <div className="vote-button" onClick={handleVote}>▲</div>
       </div>
       <div className="ml1">
@@ -49,7 +50,7 @@ function LinkItem({ link, index, showCount, history }) {
           <span className="link">({getDomain(link.url)})</span>
         </div>
         <div className="f6 lh-copy gray">
-          {link.votes.length} votes by {link.postedBy.name} {distanceInWordsToNow(link.created)}
+          {link.voteCount} votes by {link.postedBy.name} {distanceInWordsToNow(link.created)}
           {' | '}
           <Link to={`/link/${link.id}`}>
             {link.comments.length > 0 ? `${link.comments.length} comments` : 'discuss'}
--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,19 +1,32 @@
 import React from "react";
 import FirebaseContext from '../../firebase/context';
 import LinkItem from './LinkItem';
+import { LINKS_PER_PAGE } from '../../utils';
 
 function LinkList(props) {
   const { firebase } = React.useContext(FirebaseContext);
   const [links, setLinks] = React.useState([]);
   const isNewPage = props.location.pathname.includes('new');
+  const isTopPage = props.location.pathname.includes('top');
+  const [cursor, setCursor] = React.useState(null);
+  const page = Number(props.match.params.page);
 
   React.useEffect(() => {
-    geetLinks();
-  }, []);
+    const unsubscribe = getLinks();
+    return () => unsubscribe();
+  }, [isTopPage, page]);
 
-  function geetLinks() {
-    firebase.db.collection('links').orderBy('created', 'desc').onSnapshot(handleSnapshot)
+  function getLinks() {
+    const hasCursor = Boolean(cursor);
+    if (isTopPage) {
+      return firebase.db.collection('links').orderBy('voteCount', 'desc').limit(LINKS_PER_PAGE).onSnapshot(handleSnapshot)
+    } else if (page === 1) {
+      return firebase.db.collection('links').orderBy('created', 'desc').limit(LINKS_PER_PAGE).onSnapshot(handleSnapshot)
+    } else if (hasCursor) {
+      return firebase.db.collection('links').orderBy('created', 'desc').startAfter(cursor.created).limit(LINKS_PER_PAGE).onSnapshot(handleSnapshot)
 
+    }
+    return firebase.db.collection('links').orderBy('created', 'desc').limit(LINKS_PER_PAGE).onSnapshot(handleSnapshot)
   }
 
   function handleSnapshot(snapshot) {
@@ -21,21 +34,35 @@ function LinkList(props) {
       return { id: doc.id, ...doc.data() }
     });
     setLinks(links);
+    const lastLink = links[links.length - 1];
+    setCursor(lastLink);
   }
 
-  function renderLinks() {
-    if (isNewPage) {
-      return links;
+  function visitPreviousPage() {
+    if (page > 1) {
+      props.history.push(`/new/${page - 1}`);
     }
-    const topLinks = links.slice().sort((l1, l2) => l2.votes.length - l1.votes.length);
-    return topLinks;
   }
+
+  function visitNextPage() {
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`);
+    }
+  }
+
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0;
 
   return (
     <div>
-      {renderLinks().map((link, index) => (
-        <LinkItem key={link.id} showCount={true} link={link} index={index + 1} />
+      {links.map((link, index) => (
+        <LinkItem key={link.id} showCount={true} link={link} index={index + pageIndex} />
       ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitPreviousPage}>Previous</div>
+          <div className="pointer" onClick={visitNextPage}>Next</div>
+        </div>
+      )}
     </div>
   )
 }
