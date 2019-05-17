a46705d5ecd2d7a87265364c0bdad696a5a4ce86
Tetyana Shvets <tshvets@hotmail.com>
2019-05-16 18:10:03 -0700

    Firebase authentication

--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,21 +1,21 @@
 import React from "react";
 
-function useFormValidation(initialState, validate) {
-    const [values, setValues] = React.useState(initialState)
-    const [errors, setErrors] = React.useState({})
-    const [isSubmitting, setSubmitting] = React.useState(false)
+function useFormValidation(initialState, validate, authenticate) {
+    const [values, setValues] = React.useState(initialState);
+    const [errors, setErrors] = React.useState({});
+    const [isSubmitting, setSubmitting] = React.useState(false);
 
     React.useEffect(() => {
         if (isSubmitting) {
             const noErrors = Object.keys(errors).length === 0;
             if (noErrors) {
-                console.log('authenticated', values);
-                setSubmitting(false)
+                authenticate();
+                setSubmitting(false);
             } else {
-                setSubmitting(false)
+                setSubmitting(false);
             }
         }
-    }, [errors])
+    }, [errors]);
 
     function handleChange(event) {
         event.persist();
@@ -34,10 +34,17 @@ function useFormValidation(initialState, validate) {
         event.preventDefault();
         const validationErrors = validate(values);
         setErrors(validationErrors);
-        setSubmitting(true)
+        setSubmitting(true);
     }
 
-    return { handleSubmit, handleBlur, handleChange, values, errors, isSubmitting };
+    return {
+        handleSubmit,
+        handleBlur,
+        handleChange,
+        values,
+        errors,
+        isSubmitting
+    };
 }
 
 export default useFormValidation;
