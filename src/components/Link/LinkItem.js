0a36c81e5e0006394c2524c038628507e62addad
Tetyana Shvets <tshvets@hotmail.com>
2019-05-17 08:39:01 -0700

    Upvoting

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -1,14 +1,33 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import FirebaseContext from '../../firebase/context';
 import { getDomain } from '../../utils';
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
 
-function LinkItem({ link, index, showCount }) {
+function LinkItem({ link, index, showCount, history }) {
+  const { firebase, user } = React.useContext(FirebaseContext);
+
+  function handleVote() {
+    if (!user) {
+      history.push('/login');
+    } else {
+      const voteRef = firebase.db.collection('links').doc(link.id);
+      voteRef.get().then(doc => {
+        if (doc.exists) {
+          const previousVotes = doc.data().votes;
+          const vote = { votedBy: { id: user.uid, name: user.displayName } };
+          const updatedVotes = [...previousVotes, vote];
+          voteRef.update({ votes: updatedVotes });
+        }
+      })
+    }
+  }
+
   return (
     <div className="flex items-start mt2">
       <div className="flex items-center">
         {showCount && <span className="gray">{index}</span>}
-        <div className="vote-button">▲</div>
+        <div className="vote-button" onClick={handleVote}>▲</div>
       </div>
       <div className="ml1">
         <div>
@@ -26,4 +45,4 @@ function LinkItem({ link, index, showCount }) {
   )
 }
 
-export default LinkItem;
+export default withRouter(LinkItem);
